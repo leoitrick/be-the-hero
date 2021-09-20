a0bcee10bd16dcf7f492cb1b1b4537ed8903a7f5
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-09-20 15:55:00 -0400

    landing page done

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+import heroImg from "../../assets/heroes.png";
+
+function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero" />
+        <form action="">
+          <h1>Log in</h1>
+          <input placeholder="Your ID" />
+          <button className="button" type="submit">
+            Enter
+          </button>
+
+          <Link to="/register">
+            <FiLogIn size={16} color="#e02041" />I don't have an account
+          </Link>
+        </form>
+      </section>
+      <img src={heroImg} alt="Heroes" />
+    </div>
+  );
+}
+
+export default Logon;
